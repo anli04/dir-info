6e168569b05b5386e740f2077ad040a4f7befb6d
Anthony Li <ali04@stuy.edu>
2019-11-15 21:54:44 -0500

    updates

--- main.c
+++ main.c
@@ -7,36 +7,35 @@
 #include <sys/types.h>
 #include <errno.h>
 
+int dirFunc(DIR * d, char * path);
+
 int main(){
   DIR * d = opendir("Test");
   if (errno) printf("Error: %d - %s\n", errno, strerror(errno));
+  char path[] = "Test/";
+  int tsize = dirFunc(d, path);
+  closedir(d);
+  return 0;
+}
+
+int dirFunc(DIR * d, char * path){
   struct dirent *p;
   p = readdir(d);
+  long int tsize = 0;
   while (p){
     printf("Name: %s. Type: %d. ", p->d_name, p->d_type);
     struct stat f;
-    char s[] = "Test/";
+    char s[256];
+    strcpy(s, path);
     stat(strcat(s, p->d_name), &f);
     printf("Size: %ld.\n", f.st_size);
+    if (p->d_type != 4) tsize += f.st_size;
+    else {
+      DIR * d2 = opendir(s);
+      tsize += dirFunc(d2, s);
+      closedir(d2);
+    }
     p = readdir(d);
   }
-  closedir(d);
-  return 0;
+  return tsize;
 }
-/*
-int main(){
-  DIR * d = opendir("directory1");
-  if (errno) printf("Error: %d - %s\n", errno, strerror(errno));
-  struct dirent *p = readdir(d);
-  while (p){
-    struct stat f;
-    char s[256];
-    sprintf(s, "directory1/%s", p->d_name);
-    stat(s, &f);
-    printf("File name: %s. Type: %d. Size: %ld\n", p->d_name, p->d_type, f.st_size);
-    p = readdir(d);
-  }
-  closedir(d);
-  return 0;
-}
-*/
