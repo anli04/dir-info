0ae767c4ce96a43d09a79be5cf5fe9886e7988b5
Anthony Li <ali04@stuy.edu>
2019-11-15 22:36:15 -0500

    complete

--- main.c
+++ main.c
@@ -36,8 +36,8 @@ long int dirFunc(DIR * d, char * path){
     if (errno) printf("Error: %d - %s\n", errno, strerror(errno));
     printf("Size: ");
     printSize(f.st_size);
-    if (p->d_type != 4 || strncmp(p->d_name, ".", 1) == 0) tsize += f.st_size;
-    else {
+    if (p->d_type != 4) tsize += f.st_size;
+    else if (strncmp(p->d_name, ".", 1) != 0) {
       DIR * d2 = opendir(s);
       if (errno) printf("Error: %d - %s\n", errno, strerror(errno));
       strcat(s, "/");
