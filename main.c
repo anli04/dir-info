a811454c1f1ba374cfcc70735acdb3fc5fb3b4fe
Anthony Li <ali04@stuy.edu>
2019-11-15 21:39:19 -0500

    testing

--- main.c
+++ main.c
@@ -10,15 +10,33 @@
 int main(){
   DIR * d = opendir("Test");
   if (errno) printf("Error: %d - %s\n", errno, strerror(errno));
-  struct dirent *f;
-  f = readdir(d);
-  while (f){
-    printf("Name: %s.Type: %d. ", f->d_name, f->d_type);
-    struct stat s;
-    stat(f->d_name, &s);
-    printf("Size: %ld.\n", s.st_size);
-    f = readdir(d);
+  struct dirent *p;
+  p = readdir(d);
+  while (p){
+    printf("Name: %s. Type: %d. ", p->d_name, p->d_type);
+    struct stat f;
+    char s[] = "Test/";
+    stat(*strcat(s, p->d_name), &f);
+    printf("Size: %ld.\n", f.st_size);
+    p = readdir(d);
   }
   closedir(d);
   return 0;
 }
+/*
+int main(){
+  DIR * d = opendir("directory1");
+  if (errno) printf("Error: %d - %s\n", errno, strerror(errno));
+  struct dirent *p = readdir(d);
+  while (p){
+    struct stat f;
+    char s[256];
+    sprintf(s, "directory1/%s", p->d_name);
+    stat(s, &f);
+    printf("File name: %s. Type: %d. Size: %ld\n", p->d_name, p->d_type, f.st_size);
+    p = readdir(d);
+  }
+  closedir(d);
+  return 0;
+}
+*/
